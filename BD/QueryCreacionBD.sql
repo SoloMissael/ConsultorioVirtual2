55b5f38c1a4f857d682af6b2f4fc28a8caedbe9f
carlo <carlo@DESKTOP-1J8P3O3>
2017-04-05 14:38:54 -0600

    ventana asistente

--- BD/QueryCreacionBD.sql
+++ BD/QueryCreacionBD.sql
@@ -12,8 +12,30 @@ CREATE TABLE usuarios(
     contraseña VARCHAR(100)
 );
 
+CREATE TABLE citas(
+	fecha date,
+    hora varchar(5),
+    paciente varchar(100),
+    doctor varchar(100),
+    estado bool
+);
+
+insert into citas 
+values ('2017-4-04','10:30','Rodrigo Gomez','Eduardo Velazco',1),
+	   ('2017-4-04','13:30','Natalia Perez','Eduardo Velazco',1),
+	   ('2017-4-04','14:30','Alejandra Ramirez','Eduardo Velazco',1),
+	   ('2017-4-04','15:00','Diego Mayorquin','Eduardo Velazco',1),
+	   ('2017-4-04','15:30','Axel Aguirre','Eduardo Velazco',1),
+	   ('2017-4-04','16:00','Elizabeth Flores','Eduardo Velazco',1),
+	   ('2017-4-04','16:30','Margarita Garcia','Eduardo Velazco',1);
+
+
+/*aaaa-mm-dd*/
 select * from usuarios;
 
+select * from citas;
+delete from citas;
 delete FROM usuarios;
 
+drop table citas;
 SET SQL_SAFE_UPDATES = 0;
